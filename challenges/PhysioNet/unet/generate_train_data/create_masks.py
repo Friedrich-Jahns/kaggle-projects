de856ac3455fd04d16ef4a9deeb39abdd7930b60
Friedrich-Jahns <jahnsfriedrich@gmail.com>
2025-12-18 13:54:20 +0100

    save

--- challenges/PhysioNet/unet/generate_train_data/create_masks.py
+++ challenges/PhysioNet/unet/generate_train_data/create_masks.py
@@ -43,31 +43,52 @@ class mask_gererator:
         return binary_c_t, binary_t, binary_c, filtered_c
 
 
+cwd = Path(os.path.dirname(os.path.abspath(__file__)))
 
 
-dat_path = Path.cwd().parent.parent/'data_sample'
+#dat_path = Path.cwd().parent.parent/'data_sample'
+#print(dat_path)
+
+#save_path = Path.cwd().parent/ 'train_data' / 'train'
+#print(save_path)
+
+dat_path = cwd.parent.parent/'data_sample'
 print(dat_path)
 
-save_path = Path.cwd().parent/ 'train_data' / 'train'
+save_path = cwd.parent/ 'train_data' / 'train'
 print(save_path)
+
 
 for i in os.listdir(dat_path):
     filepath = dat_path / i / f'{i}-0001.png'
     data  = mask_gererator(filepath)
     curves_and_text = data.segment_curves_and_text()
-    if False:
+    if True:
         print(i)
         plt.figure(figsize=(15,15))
-        plt.subplot(221)
+        plt.subplot(331)
         plt.imshow(data.image,cmap='gray')
-        plt.subplot(222)
+        plt.title('Original')
+        plt.subplot(332)
         plt.imshow(data.image_r,cmap='gray')
-        plt.subplot(223)
+        plt.title('Red-Channel')
+        plt.subplot(333)
+        plt.imshow(data.grid,cmap='gray')
+        plt.title('Grid = Red-Channel - Blue_Channel')
+        plt.subplot(336)
+        plt.imshow(data.grid_mask(),cmap='gray')
+        plt.title('Grid_binary')
+        plt.subplot(334)
         plt.imshow(curves_and_text[1],cmap='gray')
-        plt.subplot(224)
+        plt.title('thresholded_text')
+        plt.subplot(335)
+        plt.imshow(curves_and_text[2],cmap='gray')
+        plt.title('thresholded_curve')
+        plt.subplot(338)
         plt.imshow(curves_and_text[3],cmap='gray')
+        plt.title('thresholded_curve+CC_filter')
         plt.show()
-    if True:
+    if False:
 
         mpimg.imsave(save_path / 'curve_mask' / f'{i}.png', curves_and_text[3], cmap='gray')
         mpimg.imsave(save_path / 'text_mask' / f'{i}.png', curves_and_text[1], cmap='gray')
